960eefe011a9345a272090da342fb3071b9a1e39
Vamsee Krishna Gangapatnam <s533908@nwmissouri.edu>
2019-11-19 13:40:37 -0600

    add app.app.java files

---
+++ spark-challenge/src/main/java/edu/nwmissouri/gangapatnam/App.java
@@ -0,0 +1,28 @@
+
+package edu.nwmissouri.gangapatnam;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import java.util.Arrays;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Comparator;
+
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
